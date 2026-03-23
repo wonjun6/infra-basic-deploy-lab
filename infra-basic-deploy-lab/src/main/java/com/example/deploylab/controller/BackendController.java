91ac4760887a9f32bcabc582a62935773d1821e9
WONJUN6 <cnfdornqrl20@gamil.com>
2026-03-23 11:45:43 +0900

    기능 추가

--- infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
+++ infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
@@ -16,4 +16,8 @@ public class BackendController {
 	    public String backend2() {
 	        return "backend2";
 	    }
+	    @GetMapping("/backend3")
+	    public String backend3() {
+	        return "backend3";
+	    }
 }
